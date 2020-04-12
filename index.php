76bcdaa60196b622b3237e12858f7dc809200395
Kevin Bogodo <40993902+KevinBogodo@users.noreply.github.com>
2020-04-12 11:27:16 +0100

    fix bootstrap + header & footer

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p>bonjour</p>
-  </body>
-</html>
+<?php include 'php\header.php'; ?>
+
+
+
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email/mobile</label>
+    <input type="email" class="form-control col-md-4-sm-4" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or mobile">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Je ne suis pas un robot</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+
+
+
+
+<?php include 'php\footer.php'; ?>
